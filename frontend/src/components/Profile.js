849c8cc88962baeccd83e8eac079f3b333ba3e50
Neil Pimley <neilpimley@gmail.com>
2023-02-02 15:21:45 +0000

    updated to profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -42,15 +42,15 @@ export default class Profile extends Component {
                         <div className="row">
                             <div className="col-lg-12 mx-auto">
                                 <h2>Professional Profile</h2>
-                                <p>All three of Dr McKavanagh’s degrees were from Queen's University Belfast. His primary medical degree with distinction was in 2003; his MSc in Clinical Education with distinction was in 2010; and his PhD in Cardiac CT was in 2016. He has a keen interest in research having published numerous papers and given international presentations. He was also runner-up in the European Society of Cardiology Young Investigator Award in 2013.  </p>
+                                <p>All three of Dr McKavanagh’s degrees were from Queen's University Belfast. His primary medical degree with distinction was in 2003; his MSc in Clinical Education with distinction was in 2010; and his PhD in Cardiac CT was in 2016. He has a keen interest in research having published numerous papers and given international presentations. He was also runner-up in the European Society of Cardiology Young Investigator Award in 2013.</p>
 
-                                <p>His general cardiology training was in Belfast and he completed the membership examinations for the Royal College of Physicians for both the UK and Ireland. Peter then had a two-year fellowship at a large cardiology centre in Toronto receiving recognition for this from the University of Toronto.  </p>
+                                <p>His general cardiology training was in Belfast and he completed the membership examinations for the Royal College of Physicians for both the UK and Ireland. Peter then had a two-year fellowship at a large cardiology centre in Toronto receiving recognition for this from the University of Toronto.</p>
 
-                                <p>He also has an interest in third world cardiology, having spent 6 months volunteering in Kenya in a large cardiology centre in Nairobi, as well as being involved with the Save a Heart Cardiac Surgery Nigerian Mission. He also represented Northern Ireland at junior and senior levels. He has also volunteered with vulnerable adults and offered cardiology support for the Belfast Trust homeless service. </p>
+                                <p>He also has an interest in third world cardiology, having spent 6 months volunteering in Kenya in a large cardiology centre in Nairobi, as well as being involved with the Save a Heart Cardiac Surgery Nigerian Mission. He has also volunteered with vulnerable adults and offered cardiology support for the Belfast Trust homeless service.</p>
 
-                                <p>Peter has always had a keen interest in sport, and continues to train regularly. He was captain of the Queen’s University Belfast Judo Club during this undergraduate degree and was part of the team that won five consecutive Irish University titles.  </p>
+                                <p>Peter has always had a keen interest in sport, and continues to train regularly. He was captain of the Queen’s University Belfast Judo Club during his undergraduate degree and was part of the team that won five consecutive Irish University titles. He also represented Northern Ireland at junior and senior levels. </p>
 
-                                <p>Peter returned to Northern Ireland in 2017 and has been working between the Ulster and Royal Victoria Hospitals since. He has regular cardiology clinics and is happy to see all adult cardiac disease complaints. </p>
+                                <p>Peter returned to Northern Ireland in 2017 and has been working between the Ulster and Royal Victoria Hospitals since. He has regular cardiology clinics and is happy to see all adult cardiac disease complaints.</p>
                             </div>
                         </div>
                     </div>
